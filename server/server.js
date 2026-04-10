0ba63e880e0d693cec1bb412a1edb6f44b20786b
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-04-10 19:18:17 +0530

    added root directory to server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const connectToDB = require('./config/db');
 
 connectToDB(); //DB connection
 
+
+app.get('/', (req, res) => {
+  res.send('hello this is home!');
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
