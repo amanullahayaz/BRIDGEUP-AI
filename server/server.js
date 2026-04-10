240ec8e36eac69687b7eb4038ee2f3942722a93b
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-04-10 20:12:26 +0530

    test deployement

--- server/server.js
+++ server/server.js
@@ -1,24 +1,21 @@
 require("dotenv").config();
 
 const app = require('./app');
-const connectToDB = require('./config/db');
+// const connectToDB = require('./config/db');
 
-let isConnected = false;
-
-async function connectDBOnce() {
-  if (!isConnected) {
-    await connectToDB();
-    isConnected = true;
-  }
-}
+// let isConnected = false;
+// 
+// async function connectDBOnce() {
+//   if (!isConnected) {
+//     await connectToDB();
+//     isConnected = true;
+//   }
+// }
 
 app.get('/', async (req, res) => {
-  try {
-    await connectDBOnce();
+  
     res.send('hello this is home!');
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+  
 });
 
 module.exports = app;
