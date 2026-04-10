396e89338a04bf4dee5a9dad9773cea86886e0ff
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-04-10 19:56:54 +0530

    update MongoDB connection URI to include database name

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToDB(){
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}/bridgeupai`);
     console.log('Connected to MongoDB');
     
   } catch (error) {
