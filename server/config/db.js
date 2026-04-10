47767e090b3f9c17745bf2368be4ce1c8695c98e
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-04-10 20:51:47 +0530

    corrected mongo uri

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToDB(){
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}/bridgeupai`);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
     
   } catch (error) {
