080bda9a6eaf72a9f788864adc3afa2ef0ee5c86
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-04-11 17:31:16 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,30 @@
 require("dotenv").config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-const app = require('./app');
 const connectToDB = require('./config/db');
+
+// require all the routes here
+const authRouter = require('./routes/auth.routes');
+const interviewRouter = require('./routes/interview.routes');
+
+const app = express();
+app.set('trust proxy', 1);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: function(origin, callback) {
+        const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+        if (!origin || origin === frontendUrl || origin.endsWith('.vercel.app') || origin.startsWith('http://localhost:')) {
+            callback(null, origin); // Access-Control-Allow-Origin will correctly respond with the incoming origin
+        } else {
+            callback(new Error('Blocked by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 connectToDB().then(() => {
   console.log("Database connected");
@@ -9,10 +32,12 @@ connectToDB().then(() => {
   console.error("Database connection failed", err);
 });
 
+// using all the routes here
+app.use('/api/auth', authRouter);
+app.use('/api/interview', interviewRouter);
+
 app.get('/', async (req, res) => {
-
   res.send('hello this is home!');
-
 });
 
 const PORT = process.env.PORT || 3000;
